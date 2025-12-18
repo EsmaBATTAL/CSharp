3fd95bfec689783a96cf1fd7b47a7077cce34bc2
Esma BATTAL <106597738+EsmaBATTAL@users.noreply.github.com>
2025-12-18 13:07:04 +0000

    ilkcsaharpprojesi

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DegiskenlerOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int deger = 2;
+            string degisken = null;
+            string Degisken = null;
+
+            byte b = 5;
+            sbyte c = 5;
+
+            short s = 5;
+            ushort s2 = 5;
+
+            Int16 i16 = 2;
+            int i = 2;
+            Int32 i32 = 2;
+            Int64 i64 = 2;
+
+            uint ui = 4;
+            long l = 4;
+            ulong ul = 4;
+
+            float f = 4.2f;
+            double d = 4.8;
+            decimal de = 4.8M;
+
+
+            char ch = 'c';
+            string str = "deneme";
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime);
+        }
+    }
+}
