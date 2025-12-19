5fe6262ffabc7e5382e21ca35c1d711b1974e3da
Esma BATTAL <106597738+EsmaBATTAL@users.noreply.github.com>
2025-12-19 18:41:52 +0300

    arrayMetodlari

---
+++ arrayMetodlari/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace arrayMetodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi ={23,3,42,54,7,78,46,32};
+            Console.WriteLine("***** Sırasız Dizi *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Array.Sort(sayiDizisi);
+            Console.WriteLine("***** Sıralı Dizi *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("***** Array Clear *****");
+            Array.Clear(sayiDizisi,2,2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+             Array.Reverse(sayiDizisi);
+            Console.WriteLine("***** Array Reverse *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+             
+            Console.WriteLine("***** Array Indexof *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,42));
+
+            Console.WriteLine("***** Array Resize *****");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=67;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
