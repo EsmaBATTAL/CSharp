5cb29af1483628341df87802c093d5d57b004679
Esma BATTAL <106597738+EsmaBATTAL@users.noreply.github.com>
2025-12-22 19:39:51 +0300

    enum

---
+++ enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace enumCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+
+            int sicaklik = 32;
+            if(sicaklik <=(int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if(sicaklik >=(int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.CokSicak)
+                Console.WriteLine("Hadi dışarıya çıkalım.");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal=20,
+        Sicak =25,
+        CokSicak =30
+    }
+}
